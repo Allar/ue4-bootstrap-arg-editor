3a720120d9a2c6d1488d72a9d7c6b84655e728f7
Allar <allar@michaelallar.com>
2017-10-16 23:51:06 -0700

    Initial commit

---
+++ BootstrapResourceArgUpdater/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+// This is a bastardization of what Epic does to edit resources in the bootstrap exe
+
+namespace BootstrapResourceArgUpdater
+{
+    public class ModuleResourceUpdate : IDisposable
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr BeginUpdateResource(string pFileName, [MarshalAs(UnmanagedType.Bool)]bool bDeleteExistingResources);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool UpdateResource(IntPtr hUpdate, IntPtr lpType, IntPtr lpName, ushort wLanguage, IntPtr lpData, uint cbData);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool EndUpdateResource(IntPtr hUpdate, bool fDiscard);
+
+        IntPtr UpdateHandle;
+        List<IntPtr> UnmanagedPointers = new List<IntPtr>();
+
+        public ModuleResourceUpdate(string OutputFile)
+        {
+            UpdateHandle = BeginUpdateResource(OutputFile, false);
+        }
+
+        public void SetData(int ResourceId, int Type, byte[] Data)
+        {
+            IntPtr UnmanagedPointer = Marshal.AllocHGlobal(Data.Length);
+            UnmanagedPointers.Add(UnmanagedPointer);
+
+            Marshal.Copy(Data, 0, UnmanagedPointer, Data.Length);
+
+            if (!UpdateResource(UpdateHandle, new IntPtr(Type), new IntPtr(ResourceId), 1033, UnmanagedPointer, (uint)Data.Length))
+            {
+                throw new Exception("Couldn't update resource");
+            }
+        }
+
+        public void Dispose()
+        {
+            EndUpdateResource(UpdateHandle, false);
+            foreach (IntPtr UnmanagedPointer in UnmanagedPointers)
+            {
+                Marshal.FreeHGlobal(UnmanagedPointer);
+            }
+            UnmanagedPointers.Clear();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 2)
+            {
+                Console.WriteLine("Usage: [bootstrap filename] [new arg string]");
+                return;
+            }
+
+            using (ModuleResourceUpdate Update = new ModuleResourceUpdate(args[0]))
+            {
+                Update.SetData(202, 10, Encoding.Unicode.GetBytes(args[1] + "\0"));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ue4-bootstrap-arg-editor
 Edits the command line arguments contained in a UE4 bootstrap file
+
+# Usage
+
+Usage: [bootstrap filename] [new arg string]
+
+# Example
+
+BootstrapResourceArgUpdater GenericShooterServer.exe "../../GenericShooter.uproject -log"
